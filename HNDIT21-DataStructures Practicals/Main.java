92ae6e626f8405d4a2eef908953a38e4bf48de27
Adithya Bandara <104627196+AdithyaBandara@users.noreply.github.com>
2023-11-19 18:27:08 +0530

    Here is HNDIT DSA

---
+++ HNDIT21-DataStructures Practicals/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author Adithya
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("To Run Go Class File and Run It Individualy \nOr Press Shift key + F6 key in your class file");
+    }
+    
+}
